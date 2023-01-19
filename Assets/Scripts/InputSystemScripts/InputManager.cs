f5f21464d38ebef0f3567621a1b67b445dc932a4
Sebastian Salin <35302597+sebastiansalin@users.noreply.github.com>
2023-01-20 01:58:54 +0200

    Refactor InputManager comments and var name

--- Assets/Scripts/InputSystemScripts/InputManager.cs
+++ Assets/Scripts/InputSystemScripts/InputManager.cs
@@ -12,7 +12,7 @@ public class InputManager : NetworkBehaviour
     private PlayerMotor motor;
     private PlayerLook look;
 
-    // Start is called before the first frame update
+    // Awake is called when the script instance is being loaded.
     void Awake() {
         playerInput = new PlayerControls();
         onFoot = playerInput.OnFoot;
@@ -24,17 +24,15 @@ public class InputManager : NetworkBehaviour
         
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    // Frame-rate independent FixedUpdate message for physics calculations. https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+    void FixedUpdate() {
         // Tell the playermotor to move using the value from ouir movement action.
         motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
     
     private void LateUpdate() {
-        Vector2 looky = onFoot.Look.ReadValue<Vector2>();
-        look.ProcessLook(looky);
-        
+        Vector2 lookDirection = onFoot.Look.ReadValue<Vector2>();
+        look.ProcessLook(lookDirection);
     }
 
     private void OnEnable() {
